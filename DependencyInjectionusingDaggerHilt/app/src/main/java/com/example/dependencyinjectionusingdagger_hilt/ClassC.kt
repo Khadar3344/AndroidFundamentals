00896f7ec406bfbb075835e7ae3ecb1845d2172b
Khadar Mustafe <79355446+Khadar3344@users.noreply.github.com>
2024-10-14 11:32:05 +0300

    Manual constructor injection

--- DependencyInjectionusingDaggerHilt/app/src/main/java/com/example/dependencyinjectionusingdagger_hilt/ClassC.kt
+++ DependencyInjectionusingDaggerHilt/app/src/main/java/com/example/dependencyinjectionusingdagger_hilt/ClassC.kt
@@ -2,23 +2,32 @@ package com.example.dependencyinjectionusingdagger_hilt
 
 import android.util.Log
 
-class ClassC {
-
-//    private val classA = ClassA()
+//class ClassC() {
+//
+//    /*private val classA = ClassA()
 //    private val classB = ClassB()
 //
 //    fun startClassC() {
 //        classA.startClassA()
 //        classB.startClassB()
 //        Log.i("TAG", "ClassC is starting...")
-//    } Without DI
+//    }*/     //Without DI
+//
+//    /*lateinit var classA: ClassA
+//    lateinit var classB: ClassB
+//
+//    fun startClassC() {
+//        classA.startClassA()
+//        classB.startClassB()
+//        Log.i("TAG", "ClassC is starting...")
+//    }*/     // field injection
+//
+//}
 
-    lateinit var classA: ClassA
-    lateinit var classB: ClassB
-
+class ClassC(private val classA: ClassA, private val classB: ClassB) {
     fun startClassC() {
         classA.startClassA()
         classB.startClassB()
         Log.i("TAG", "ClassC is starting...")
     }
-}
+} // constructor injection
--- DependencyInjectionusingDaggerHilt/app/src/main/java/com/example/dependencyinjectionusingdagger_hilt/MainActivity.kt
+++ DependencyInjectionusingDaggerHilt/app/src/main/java/com/example/dependencyinjectionusingdagger_hilt/MainActivity.kt
@@ -12,12 +12,17 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-//        val classC = ClassC()
-//        classC.startClassC() Without DI
+        /*val classC = ClassC()
+        classC.startClassC()*/ //Without DI
 
-        val classC = ClassC()
+        /*val classC = ClassC()
         classC.classA = ClassA()
         classC.classB = ClassB()
-        classC.startClassC() // field injection
+        classC.startClassC()*/ // field injection
+
+        val classA = ClassA()
+        val classB = ClassB()
+        val classC = ClassC(classA, classB)
+        classC.startClassC() // constructor injection
     }
 }
